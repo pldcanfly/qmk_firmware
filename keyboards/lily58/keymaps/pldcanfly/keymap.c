0b49213975f153f629899a5fa6c241d996bb578c
pldcanfly <pldcanfly@gmail.com>
2024-08-02 19:12:00 +0200

    fix: removed keylogger from oled
    no i do not want to show my passwords on my keyboard thanks

--- keyboards/lily58/keymaps/pldcanfly/keymap.c
+++ keyboards/lily58/keymaps/pldcanfly/keymap.c
@@ -38,7 +38,7 @@ const char *read_layer_state(void);
 const char *read_logo(void);
 void set_keylog(uint16_t keycode, keyrecord_t *record);
 const char *read_keylog(void);
-const char *read_keylogs(void);
+// const char *read_keylogs(void);
 
 // const char *read_mode_icon(bool swap);
 // const char *read_host_led_state(void);
@@ -50,8 +50,8 @@ bool oled_task_user(void) {
     // If you want to change the display of OLED, you need to change here
     oled_write_ln(read_layer_state(), false);
     oled_write_ln(read_keylog(), false);
-    oled_write_ln(read_keylogs(), false);
-    //oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
+    // oled_write_ln(read_keylogs(), false);
+    // oled_write_ln(read_mode_icon(keymap_config.swap_lalt_lgui), false);
     //oled_write_ln(read_host_led_state(), false);
     //oled_write_ln(read_timelog(), false);
   } else {
